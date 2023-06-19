1c7bb97a263337226a078afbe9d1d15ee71f2c98
Suyog-007 <103415987+Suyog-007@users.noreply.github.com>
2023-06-20 00:06:16 +0530

    Add files via upload

--- example.sh
+++ example.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-#Author : devraj
+
 #Script is as follows
 
 # Group into Tar
-##tar -zvcf new.tar test_folder/
+##tar -zvcf new.tar testfolder/
 
 # Encrypt the data using key
 # openssl enc -in input.txt -out encrypted.enc -pbkdf2 -e -aes256 -k keyfile.key
-##openssl enc -a -in new.tar -out dir.aes256 -pbkdf2 -e -aes256 -k keyfile.key
+openssl enc -a -in bankdataset.csv -out encrypted.txt -pbkdf2 -e -aes256 -k sym.key
 #openssl aes-256-cbc -a -salt -pbkdf2 -in new.tar -out dir.aes256
 
 # Send file over SSH
-##sudo scp -i "cloud.pem" dir.aes256 ubuntu@ec2-13-236-147-242.ap-southeast-2.compute.amazonaws.com:/home/ubuntu/dir.aes256
-##echo "File uploaded to cloud"
+sudo scp encrypted.txt sym.key devraj@192.168.128.227:/home/devraj
+echo "File sent"
 
 # Entering AWS Server
-##sudo ssh -t -t -i "cloud.pem" ubuntu@ec2-13-236-147-242.ap-southeast-2.compute.amazonaws.com << EOF
-openssl enc -a -in dir2.aes256 -out decrypted.tar -pbkdf2 -d -aes256 -k sym.key
-tar --one-top-level -xvf decrypted.tar
-##aws s3 mv decrypted/test_folder s3://inputdatafromec2 --recursive
+# sudo ssh -t -t -i "ec2key.pem" ubuntu@ec2-13-50-250-17.eu-north-1.compute.amazonaws.com << EOF
+# openssl enc -a -in dir.aes256 -out decrypted.tar -pbkdf2 -d -aes256 -k keyfile.key
+# tar --one-top-level -xvf decrypted.tar
+# aws s3 mv decrypted/testfolder s3://suyogs3bucket1 --recursive
